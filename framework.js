ef1ba390cdb3517f512b7313efc7094e08b2956a
--global <--global>
2026-04-20 11:56:35 +0800

    added API call

--- framework.js
+++ framework.js
@@ -130,31 +130,4 @@ window.addEventListener('message', (event) => {
     }
     const type = event.data?.type;
 
-    if (type === 'GET_USER_PRESENCE') {
-
-        console.log("Handling presence request...");
-        const platformClient = window.platformClient
-        const client = platformClient.ApiClient.instance;
-        client.setEnvironment(platformClient.PureCloudRegionHosts.us_west_2); // Genesys Cloud region
-
-        // Manually set auth token or use loginImplicitGrant(...) or loginClientCredentialsGrant(...) or loginPKCEGrant(...)
-        client.setAccessToken(window.authToken);
-
-        let apiInstance = new platformClient.PresenceApi();
-        let userId = "a7a42796-82c9-4016-8281-5406977c42a3"; // String | user Id
-
-        // Get a user's Genesys Cloud presence.
-        apiInstance.getUserPresencesPurecloud(userId)
-        .then((data) => {
-            console.log(`getUserPresencesPurecloud success! data: ${JSON.stringify(data, null, 2)}`);
-            document.getElementById("apiResult").innerHTML =`<pre>${JSON.stringify(data, null, 2)}</pre>`;
-
-        })
-        .catch((err) => {
-            console.log("There was a failure calling getUserPresencesPurecloud");
-            document.getElementById("apiResult").innerHTML ="Error fetching data";
-            console.error(err);
-        });
-        }
-
 });
